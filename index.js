8023eb71f8038d22d6f3e63e1790daf43ccccbc1
thommccarthy <mccarttj@gmail.com>
2021-09-29 22:07:41 -0400

    added basic database functionality and submit button

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const { response } = require('express');
 const express = require('express');
+const { response } = require('express');
+const Datastore = require('nedb');
 //bring express package in as a function
 const app = express();
+
 app.listen(5000, () => console.log('listening at 5000'));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static('public'));
 
-//
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+//if a post request is sent to the route '/api', respond with an object in json format. it's friendly to include a status message.
 app.post('/api', (request, response) => {
   console.log('I got a request');
-  console.log(request.body);
   const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  console.log(data);
+  database.insert(data);
   response.json({
     status: 'success',
+    timestamp: timestamp,
     latitude: data.lat,
     longitude: data.long,
   });
